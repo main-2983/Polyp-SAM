dc4ba3da9138ea49888a2026bac3068f0d34d7c4
main-2983 <nguyen.mai@sun-asterisk.com>
2023-08-10 16:22:03 +0700

    update model to support Accelerate

---
+++ src/models.py
@@ -0,0 +1,100 @@
+from typing import Tuple, List, Dict, Any
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from segment_anything.modeling import MaskDecoder, ImageEncoderViT, PromptEncoder
+
+
+class PolypSAM(nn.Module):
+    """
+    A wrapper to replace the normal SAM with its own forward function
+    """
+    def __init__(self,
+                 image_encoder: ImageEncoderViT,
+                 mask_decoder: MaskDecoder,
+                 prompt_encoder: PromptEncoder,
+                 pixel_mean: List[float] = [0.485, 0.456, 0.406],
+                 pixel_std: List[float] = [0.229, 0.224, 0.225]):
+        super(PolypSAM, self).__init__()
+        self.image_encoder = image_encoder
+        self.mask_decoder = mask_decoder
+        self.prompt_encoder = prompt_encoder
+        self.register_buffer("pixel_mean", torch.Tensor(pixel_mean).view(-1, 1, 1), False)
+        self.register_buffer("pixel_std", torch.Tensor(pixel_std).view(-1, 1, 1), False)
+
+    def forward(self,
+                input: Dict[str, Any],
+                multimask_output: bool = False):
+        """
+        This forward function does not accept batch input
+        """
+
+        image = input.get("image")
+
+        image_embedding = self.image_encoder(image[None])
+
+        points = input.get("point_prompt")
+        sparse_embeddings, dense_embeddings = self.prompt_encoder(
+            points=points,
+            boxes=input.get("box_prompt", None),
+            masks=input.get("mask_input", None),
+        )
+        low_res_masks, iou_predictions = self.mask_decoder(
+            image_embeddings=image_embedding,
+            image_pe=self.prompt_encoder.get_dense_pe(),
+            sparse_prompt_embeddings=sparse_embeddings,
+            dense_prompt_embeddings=dense_embeddings,
+            multimask_output=multimask_output,
+        )
+        mask = self.postprocess_masks(
+            low_res_masks,
+            input_size=image.shape[-2:],
+            original_size=input.get("image_size"),
+        )
+
+        return mask
+
+    def postprocess_masks(
+            self,
+            masks: torch.Tensor,
+            input_size: Tuple[int, ...],
+            original_size: Tuple[int, ...],
+    ) -> torch.Tensor:
+        """
+        Remove padding and upscale masks to the original image size.
+
+        Arguments:
+          masks (torch.Tensor): Batched masks from the mask_decoder,
+            in BxCxHxW format.
+          input_size (tuple(int, int)): The size of the image input to the
+            model, in (H, W) format. Used to remove padding.
+          original_size (tuple(int, int)): The original size of the image
+            before resizing for input to the model, in (H, W) format.
+
+        Returns:
+          (torch.Tensor): Batched masks in BxCxHxW format, where (H, W)
+            is given by original_size.
+        """
+        masks = F.interpolate(
+            masks,
+            (self.image_encoder.img_size, self.image_encoder.img_size),
+            mode="bilinear",
+            align_corners=False,
+        )
+        masks = masks[..., : input_size[0], : input_size[1]]
+        masks = F.interpolate(masks, original_size, mode="bilinear", align_corners=False)
+        return masks
+
+    def preprocess(self, x: torch.Tensor) -> torch.Tensor:
+        """Normalize pixel values and pad to a square input."""
+        # Normalize colors
+        x = (x - self.pixel_mean) / self.pixel_std
+
+        # Pad
+        h, w = x.shape[-2:]
+        padh = self.image_encoder.img_size - h
+        padw = self.image_encoder.img_size - w
+        x = F.pad(x, (0, padw, 0, padh))
+        return x
