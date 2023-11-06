c424f27442b9850a60635c5e1f99d8394ba3bbc1
Thanh Dang <dang.hong.thanh@sun-asterisk.com>
2023-11-06 04:08:43 +0000

    add some code

--- src/models/SelfPromptBox/box_prompt_SAM.py
+++ src/models/SelfPromptBox/box_prompt_SAM.py
@@ -10,6 +10,8 @@ from typing import Any, Dict, List, Tuple
 from src.models.SelfPromptBox.detection_head import DetectionHead
 from src.models.SelfPromptBox.position_encoding import *
 from src.models.iterative_polypSAM import IterativePolypSAM
+from torch.nn import functional as F
+from tools.box_ops import box_cxcywh_to_xyxy
 
 class SelfBoxPromptSam(IterativePolypSAM):
     def __init__(
@@ -56,3 +58,32 @@ class SelfBoxPromptSam(IterativePolypSAM):
         out=self.box_decoder(image_embeddings.detach(),pos=pos_embedding)
         return out 
     
+class PostProcess(nn.Module):
+    """ This module converts the model's output into the format expected by the coco api"""
+    @torch.no_grad()
+    def forward(self, outputs, target_sizes):
+        """ Perform the computation
+        Parameters:
+            outputs: raw outputs of the model
+            target_sizes: tensor of dimension [batch_size x 2] containing the size of each images of the batch
+                          For evaluation, this must be the original image size (before any data augmentation)
+                          For visualization, this should be the image size after data augment, but before padding
+        """
+        out_logits, out_bbox = outputs['pred_logits'], outputs['pred_boxes']
+
+        assert len(out_logits) == len(target_sizes)
+        assert target_sizes.shape[1] == 2
+
+        prob = F.softmax(out_logits, -1)
+        scores, labels = prob[..., :-1].max(-1)
+
+        # convert to [x0, y0, x1, y1] format
+        boxes = box_cxcywh_to_xyxy(out_bbox)
+        # and from relative [0, 1] to absolute [0, height] coordinates
+        img_h, img_w = target_sizes.unbind(1)
+        scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1)
+        boxes = boxes * scale_fct[:, None, :]
+
+        results = [{'scores': s, 'labels': l, 'boxes': b} for s, l, b in zip(scores, labels, boxes)]
+
+        return results
