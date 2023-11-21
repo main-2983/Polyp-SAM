83cb9a292e3a94d59ce8b0ca7890aca238f7dd1a
main-2983 <nguyen.mai@sun-asterisk.com>
2023-11-21 12:13:31 +0700

    add Mlvl

--- src/models/SelfPromptPoint/__init__.py
+++ src/models/SelfPromptPoint/__init__.py
@@ -1,3 +1,3 @@
 from .self_point_prompt import SelfPointPromptSAM, SelfPointPromptSAMv2, PointGenModel
 from .point_gen_module import PointGenModule, PointGenModulev2
-from .point_head import PointHead
+from .point_head import PointHead, MlvlPointHead
